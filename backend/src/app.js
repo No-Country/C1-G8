ee0508e4edc1533dd408d5e16c2b137480a0414b
German <rodriguezgermancarlos@gmail.com>
2021-11-15 15:42:36 -0300

    creacion de servidor

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express' )
+const cors = require('cors' )
+const morgan = require('morgan' )
+const app = express() 
+
+
+//setting config
+app.set('name', 'server')
+app.set('port', process.env.PORT || 4000)
+
+
+
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(cors())
+app.use(morgan('dev'))
+
+//routes
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+
+module.exports = app
+
