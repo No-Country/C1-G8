093de5122009b51b9f69a9ab6ca8d0fb87668e3d
German <rodriguezgermancarlos@gmail.com>
2021-11-29 17:37:58 -0300

    ajuste de wallet

--- backend/src/controller/wallet.controller.js
+++ backend/src/controller/wallet.controller.js
@@ -1,4 +1,6 @@
 const Users = require('../model/User') 
+const axios = require('axios')
+
 
 const buycontrollers = async (req, res) => {  
     try{   
@@ -18,7 +20,23 @@ const viewcontrollers = async (req, res) => {
     try{
         const {id} = req.params
         const user = await  Users.findById(id)
+    //    const myWallet = []
         
+    //     user.wallet.map ( async   (item, index ) =>  {
+    
+    // try {
+    //     const {data} = await axios.get(`https://api.coingecko.com/api/v3/coins/${item.name}`)
+    //     myWallet.push
+    //     ({name: data.name, 
+    //      quantity: item.quantity, 
+    //      price: data.market_data.current_price.usd,
+    //      Image: data.image.small,    
+    //     })
+    // } catch (error) {
+    //     myWallet.push("error")
+    // }       
+    //     })
+   
         res.json(user.wallet)
 
     }catch{ 
