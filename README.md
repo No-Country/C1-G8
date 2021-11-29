6763bedea8e24e5fbb9de961bebde7691ad64fb4
Mégas Vanzillotta <megas.vanzi@gmail.com>
2021-11-29 11:41:32 -0300

    add MUI table

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # crypto-app-g8-c1
 
-Features:	
-Consumir api de coingecko		
+Features:
 
-		Preparar fetch / get general	ok
-		Preparar fetch / get específico de cada moneda	ok
-			
-	Ver cotizaciones de distintas criptomonedas		
-		Tener un board en donde ver las distintas cotizaciones	ok
-		Ver índices, indicadores	ok
-			
-	Ver detalles de cada moneda		
-		Generar rutas (ej: /bitcoin, /ethereum)	ok
-		Tener una vista por cada moneda específica	
-			
-	Registrarse y loguearse con usuario		
-		Armar formulario de registro	
-		Validar formulario	
-		Encriptar contraseña	
-		Registrar usuario en base de datos	
-		Armar login	
-		Usar JWT para generar token	
-		Validar logueo	
-		Enviar token para la sesión del usuario	
+    Consumir api de coingecko	ok
+
+    	Preparar fetch / get general	ok
+    	Preparar fetch / get específico de cada moneda	ok
+
+    Ver cotizaciones de distintas criptomonedas
+    	Tener un board en donde ver las distintas cotizaciones	ok
+    	Ver índices, indicadores	ok
+
+    Ver detalles de cada moneda
+    	Generar rutas (ej: /bitcoin, /ethereum)	ok
+    	Tener una vista por cada moneda específica
+
+    Registrarse y loguearse con usuario
+    	Armar formulario de registro
+    	Validar formulario
+    	Encriptar contraseña
+    	Registrar usuario en base de datos
+    	Armar login
+    	Usar JWT para generar token
+    	Validar logueo
+    	Enviar token para la sesión del usuario
--- frontend/src/components/ListCoins.jsx
+++ frontend/src/components/ListCoins.jsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
-import Crypto from "./Crypto";
 import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const ListCoins = () => {
   const [coins, setCoins] = useState([]);
@@ -23,47 +29,46 @@ const ListCoins = () => {
 
   return (
     <>
-      <table
-        style={{
-          textAlign: "center",
-          margin: 0,
-          position: "absolute",
-          left: 100,
-          right: 0,
-        }}
-      >
-        <thead>
-          <tr>
-            <td>Coin</td>
-            <td>a</td>
-            <td>Name</td>
-            <td>Price</td>
-            <td>24h Change</td>
-          </tr>
-        </thead>
-        <tbody>
-          {coins.map(
-            ({
-              image,
-              symbol,
-              name,
-              current_price,
-              price_change_percentage_24h,
-            }) => {
-              return (
-                <Crypto
-                  key={symbol}
-                  image={image}
-                  symbol={symbol}
-                  name={name}
-                  price={current_price}
-                  change={price_change_percentage_24h}
-                />
-              );
-            }
-          )}
-        </tbody>
-      </table>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 450 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Crypto </TableCell>
+              <TableCell>Symbol</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="right">Price (USD)</TableCell>
+              <TableCell align="right">24h Change</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {coins.map(
+              ({
+                image,
+                symbol,
+                name,
+                current_price,
+                price_change_percentage_24h,
+                id,
+              }) => (
+                <TableRow
+                  key={id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    <img src={image} alt={name} style={{ width: "60px" }} />
+                  </TableCell>
+                  <TableCell align="center">{symbol.toUpperCase()}</TableCell>
+                  <TableCell>{name}</TableCell>
+                  <TableCell align="right">{current_price}</TableCell>
+                  <TableCell align="right">
+                    {price_change_percentage_24h}
+                  </TableCell>
+                </TableRow>
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
